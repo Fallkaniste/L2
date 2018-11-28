b2c91149a1a98b22c711e260bc91626f8aa393e8
Romain <daspetromain@gmail.com>
2018-11-28 12:52:27 +0100

    session 28/11/2018 Démarage du projet DAI html transferer en js

---
+++ DAI/Projet/translator.js
@@ -0,0 +1,393 @@
+"use strict";
+
+// Demande le lancement de l'exécution quand toute la page Web sera chargée
+window.addEventListener('load', go);
+
+// SAM Design Pattern : http://sam.js.org/
+let actions, model, state, view;
+
+//-------------------------------------------------------------------- Init ---
+// Point d'entrée de l'application
+function go() {
+  console.log('GO !');
+
+  sandbox();
+
+  const data = {
+    authors: 'Prénom1 NOM1 et Prénom2 NOM2',
+    languagesSrc: ['fr', 'en', 'es'],
+    languagesDst: ['fr', 'en', 'es', 'it', 'ar', 'eo', 'ja', 'zh'],
+    langSrc: 'fr',
+    langDst: 'en',
+    translations : translations1,
+  };
+  actions.initAndGo(data);
+}
+
+//-------------------------------------------------------------------- Tests ---
+
+function sandbox() {
+
+  function action_display(data) {
+    // console.log(data);
+    if (!data.error) {
+      const language = languages[data.languageDst].toLowerCase();
+      console.log(`'${data.expression}' s'écrit '${data.translatedExpr}' en ${language}`);
+    } else {
+      console.log('Service de traduction indisponible...');
+    }
+  }
+
+  const expr = 'asperge';
+  const langSrc = 'fr';
+  const langDst = 'en';
+  googleTranslation(expr, langSrc, langDst, action_display );
+}
+
+//------------------------------------------------------------------ Données ---
+
+// Correspondance entre codes de langue et leur nom
+// Pour d'autres langues, voir ici :  https://fr.wikipedia.org/wiki/Liste_des_codes_ISO_639-1
+const languages = {
+  fr: 'Français',
+  en: 'Anglais',
+  es: 'Espagnol',
+  it: 'Italien',
+  ar: 'Arabe',
+  eo: 'Espéranto',
+  ja: 'Japonais',
+  zh: 'Chinois',
+};
+
+//------------------------------------------------------------------ Actions ---
+// Actions appelées dans le code HTML quand des événements surviennent
+//
+actions = {
+
+  initAndGo(data) {
+    model.samPresent({
+      do:'init',
+      authors: data.authors,
+      langSrc: data.langSrc,
+      langDst: data.langDst,
+      languagesSrc: data.languagesSrc,
+      languagesDst: data.languagesDst,
+      translations: data.translations,
+    });
+  },
+
+  // TODO: Ajouter les autres actions...
+};
+
+//-------------------------------------------------------------------- Model ---
+// Unique source de vérité de l'application
+//
+model = {
+  tabs: {
+    // TODO: propriétés pour les onglets
+  },
+  request: {
+    languagesSrc: [],
+    languagesDst: [],
+    langSrc: '',
+    langDst: '',
+    expression: '',
+  },
+  translations: {
+    values:[
+      // ['fr','Asperge','en','Asparagus'],
+    ],
+  },
+  sorted: {
+    // TODO: propriétés pour trier les colonnes
+  },
+  marked: {
+    // TODO: propriétés pour les lignes marquées pour suppression
+  },
+  pagination: {
+    // TODO: propriétés pour gérer la pagination
+  },
+  app: {
+    authors: '',
+    mode: 'main',    // in ['main', 'lang']
+    sectionId: 'app',
+  },
+
+  // Demande au modèle de se mettre à jour en fonction des données qu'on
+  // lui présente.
+  // l'argument data est un objet confectionné dans les actions.
+  // Les propriétés de data comportent les modifications à faire sur le modèle.
+  samPresent(data) {
+
+    switch (data.do) {
+
+      case 'init':
+        this.app.authors = data.authors;
+        this.request.languagesSrc = data.languagesSrc;
+        this.request.languagesDst = data.languagesDst;
+        this.request.langSrc = data.langSrc;
+        this.request.langDst = data.langDst;
+        this.translations.values = data.translations;
+        break;
+
+
+      case '':  // TODO: Autres modifications de model...
+
+        break;
+
+      default:
+        console.error(`model.samPresent(), unknown do: '${data.do}' `);
+    }
+
+    // Demande à l'état de l'application de prendre en compte la modification
+    // du modèle
+    state.samUpdate(this);
+  },
+
+};
+//-------------------------------------------------------------------- State ---
+// État de l'application avant affichage
+//
+state = {
+  tabs: {
+    // TODO: données des onglets déduites de model
+  },
+  translations: {
+    // TODO: données de traductions déduites de model (par langue notamment)
+  },
+
+  samUpdate(model) {
+
+    // TODO: Toutes les mises à jour des données pour préparer l'affichage
+
+    this.samRepresent(model);
+  },
+
+  // Met à jour l'état de l'application, construit le code HTML correspondant,
+  // et demande son affichage.
+  samRepresent(model) {
+    let representation = '';
+
+    let headerUI = view.headerUI(model,state);
+    let tabsUI = view.tabsUI(model,state);
+    let requestUI = view.requestUI(model,state);
+    let translationsUI = view.translationsUI(model,state);
+
+    representation += headerUI + tabsUI + requestUI + translationsUI;
+
+    // TODO: la représentation de l'interface est différente selon
+    //       qu'on affiche l'onglet 'Traductions' (avec formulaire de traduction)
+    //       ou qu'on affiche les onglets par langue...
+
+    representation = view.generateUI(model, this, representation);
+
+    view.samDisplay(model.app.sectionId, representation);
+  },
+};
+//--------------------------------------------------------------------- View ---
+// Génération de portions en HTML et affichage
+//
+view = {
+
+  // Injecte le HTML dans une balise de la page Web.
+  samDisplay(eltId, representation) {
+    const elt = document.getElementById(eltId);
+    elt.innerHTML = representation;
+  },
+
+  // Renvoit le HTML
+  generateUI(model, state, representation) {
+    return `
+    <div class="container">
+      ${representation}
+    </div>
+    `;
+  },
+
+  headerUI(model,state) {
+    return `
+    <section id="header" class="mt-4">
+      <div class="row mb-4">
+        <div class="col-6">
+          <h1>Traducteur</h1>
+        </div>
+        <div class="col-6 text-right align-middle">
+          <div class="btn-group mt-2">
+            <button class="btn btn-primary">Charger</button>
+            <button class="btn btn-ternary">Enregistrer</button>
+            <button class="btn btn-secondary">Préférences</button>
+            <button class="btn btn-primary">À propos</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    `;
+  },
+
+  tabsUI(model,state) {
+    return `
+    <section id="tabs">
+      <div class="row justify-content-start ml-1 mr-1">
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link active"
+              href="#">Traductions
+              <span class="badge badge-primary">4</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Français
+              <span class="badge badge-primary">3</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Anglais
+              <span class="badge badge-primary">2</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Arabe
+              <span class="badge badge-primary">1</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <select class="custom-select" id="selectFrom">
+              <option selected="selected" value="0">Autre langue...</option>
+              <option value="es">Espagnol (1)</option>
+              <option value="it">Italien (1)</option>
+            </select>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <br />
+    `
+  },
+
+  requestUI(model,state) {
+    return`
+    <section id="request">
+      <form action="">
+        <div class="form-group">
+          <fieldset class="form-group">
+            <div class="row align-items-end">
+              <div class="col-sm-3 col-5">
+                <div class="form-group">
+                  <label for="selectFrom">Depuis</label>
+                  <select class="custom-select" id="selectFrom">
+                    <option selected="selected" value="fr">Français</option>
+                    <option value="en">Anglais</option>
+                    <option value="es">Espagnol</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group col-sm-1 col-2">
+                <button class="btn btn-secondary" type="button">⇄</button>
+              </div>
+              <div class="col-sm-3 col-5">
+                <div class="form-group">
+                  <label for="selectTo">Vers</label>
+                  <select class="custom-select" id="selectTo">
+                    <option value="fr">Français</option>
+                    <option selected="selected" value="en">Anglais</option>
+                    <option value="es">Espagnol</option>
+                    <option value="it">Italien</option>
+                    <option value="ar">Arabe</option>
+                    <option value="eo">Espéranto</option>
+                    <option value="ja">Japonais</option>
+                    <option value="zh">Chinois</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-sm-5 col-12">
+                <div class="input-group mb-3">
+                  <input value="" id="expressionText" type="text" class="form-control" placeholder="Expression..." />
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="button">Traduire</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </form>
+    </section>
+    `
+  },
+
+  transGeneration(model,state){
+    let translations=[];
+    for (var i = 0; i < model.translations.values.length; i++) {
+      translations[i] = `<tr>
+        <td class="text-center text-secondary"> ${i} </td>
+        <td class="text-center">
+          <span class="badge badge-info">${model.translations.values[i][0].toLowerCase}</span>
+        </td>
+        <td>${model.translations.values[i][1].toLowerCase()}</td>
+        <td class="text-center">
+          <span class="badge badge-info">${model.translations.values[i][2]}</span>
+        </td>
+        <td>${model.translations.values[i][3].toLowerCase()}</td>
+        <td class="text-center">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" />
+          </div>
+        </td>
+      </tr>`
+    }
+
+    return(translations.join('\n'))
+  },
+
+  translationsUI(model,state) {
+    let translations = view.transGeneration(model,state);
+    return`
+    <section id="translations">
+      <fieldset>
+        <legend class="col-form-label">Traductions</legend>
+        <div class="table-responsive">
+          <table class="col-12 table table-sm table-bordered">
+            <thead>
+              <th class="align-middle text-center col-1">
+                <a href="#">N°</a>
+              </th>
+              <th class="align-middle text-center col-1">
+                <a href="#">Depuis</a>
+              </th>
+              <th class="align-middle text-center ">
+                <a href="#">Expression</a>
+              </th>
+              <th class="align-middle text-center col-1">
+                <a href="#">Vers</a>
+              </th>
+              <th class="align-middle text-center ">
+                <a href="#">Traduction</a>
+              </th>
+              <th class="align-middle text-center col-1">
+                <div class="btn-group">
+                  <button class="btn btn-ternary">Suppr.</button>
+                </div>
+              </th>
+            </thead>
+              ${translations}
+            <tr>
+              <td colspan="5" class="align-middle text-center"> </td>
+              <td class="align-middle text-center">
+                <div class="btn-group">
+                  <button class="btn btn-secondary">Suppr. tous</button>
+                </div>
+              </td>
+            </tr>
+
+            </table>
+          </div>
+
+        </fieldset>
+      </section>
+    `
+  },
+
+
+
+
+};
